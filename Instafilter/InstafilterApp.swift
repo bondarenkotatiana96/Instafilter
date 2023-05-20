035afe3c4a74a25ce95d92af90e3745a9076fb8a
Tatiana Bondarenko <62190724+bondarenkotatiana96@users.noreply.github.com>
2023-05-19 22:43:19 -0700

    Add files via upload

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Tatiana Bondarenko on 5/19/23.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
